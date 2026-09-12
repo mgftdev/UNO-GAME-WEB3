44c08d265c29c7abed5b0f924148a3c2200c7c8a
Marius Gafton <mgafton.work@gmail.com>
2026-09-12 14:27:06 +0200

    Testing
    Test Suites: 2 failed, 6 passed, 8 total
    Tests:       27 failed, 211 passed, 238 total
    Snapshots:   0 total
    Time:        3.033 s
    Ran all test suites.

---
+++ src/model/round.ts
@@ -0,0 +1,5 @@
+export * from "./round/standard-round";
+export * from "./round/play-legality";
+
+/** The tests import a `Round` type from this module. */
+export type { StandardRound as Round } from "./round/standard-round";
